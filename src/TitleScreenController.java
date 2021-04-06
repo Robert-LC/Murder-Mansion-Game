3abbdbc192c5e722ea9eb819851318147841e67c
Robert Locicero <robertlocicerojr@gmail.com>
2021-04-06 19:01:31 -0400

    Added Main Menu

---
+++ src/TitleScreenController.java
@@ -0,0 +1,14 @@
+// TitleScreenController.java - Robert LoCicero
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TitleScreenController
+{
+
+    @FXML
+    private Button newGameButton;
+
+    @FXML
+    private Button loadGameButton;
+
+}
