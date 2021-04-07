b83e3854768495aea6488f3b6cdc1fce1827a3ea
sgc6512 <81432815+sgc6512@users.noreply.github.com>
2021-04-07 18:59:45 -0400

    Updates to work with GUI

--- src/Room.java
+++ src/Room.java
@@ -1,70 +1,48 @@
-import java.awt.*;
 import java.util.ArrayList;
 
-public class Room
-{
+public class Room {
     String name;
     String desc;
+    String[] exits;
     ArrayList<Item> contents;
-    ArrayList<String> exits;
 
-    //public Room(String name, String[] exits, Item[] contents)
-   // {
-    //    this.name = name;
-    //    this.exits = exits;
-    //    this.contents = contents;
-    //}
+    public Room() {
+        contents = new ArrayList<>();
+    }
 
-    public Room()
-    {
-
+    public void addItem(Item i) {
+        contents.add(i);
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getDesc() {
         return desc;
     }
 
-    public String getExits()
-    {
-        String result = "";
-        for(String s: exits)
-        {
-            result += s + " ";
-        }
-        return result;
-    }
-
-    public String getContents()
-    {
-        String result = "";
-        for(Item i: contents)
-        {
-            result += i.getName() + " ";
-        }
-        return result;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public void setDesc(String desc)
-    {
+    public void setDesc(String desc) {
         this.desc = desc;
     }
 
-    public void setExits(ArrayList<String> exits)
-    {
+    public void setExits(String[] exits) {
         this.exits = exits;
     }
 
-    public void setContents(ArrayList<Item> contents)
-    {
+    public ArrayList<Item> getContents() {
+        return contents;
+    }
+
+    public void setContents(ArrayList<Item> contents) {
         this.contents = contents;
+    }
+
+    public String[] getExits() {
+        return exits;
     }
 }
