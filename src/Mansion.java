9b3f4b95ec8daa6a535bfbf4b118b034bf2beab2
sgc6512 <81432815+sgc6512@users.noreply.github.com>
2021-04-07 18:55:39 -0400

    Update Mansion.java to work with file reader

--- src/Mansion.java
+++ src/Mansion.java
@@ -5,21 +5,11 @@ public class Mansion {
     HashMap<String, Room> map;
     ArrayList<Room> rooms;
 
-    public Mansion()
-    {
+    public Mansion() {
         map = new HashMap<>();
         rooms = new ArrayList<>();
-        //File Read in all room info -- for now just manually create for testing
-        //Name, Description, Exits, Contents, etc.
         FileReaderClass reader = new FileReaderClass();
-        HashMap<String, Item> itemTypeHashMap = new HashMap<String, Item>();
-
-        // Reads items.txt, Creates Item Objects then stores them in a Hashmap(Item Name, Item Object)
-        itemTypeHashMap = reader.readItemsFile("Items.txt");
-
-
-        // Reads Rooms.txt, Creates room objects using the txt file, and the HashMap with items in it
-        reader.readRoomFile("Rooms.txt", itemTypeHashMap);
+        rooms = reader.readRoomFile("rooms.txt", reader.readItemsFile("Items.txt"));
         this.build();
     }
 
