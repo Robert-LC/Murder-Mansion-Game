9080cc0dfaba42075a417f0afe36d277440ddf72
meganikki2001 <82064919+meganikki2001@users.noreply.github.com>
2021-04-08 14:28:15 -0400

    Update Clue.java

--- src/Clue.java
+++ src/Clue.java
@@ -1,11 +1,18 @@
 public class Clue extends Item
 {
+    private Suspects suspect;
+    
     // Constructor for Clues calls the Item Constructor.
     Clue(String name, String description, String roomLocation)
     {
         super(name, description, roomLocation);
     }
-
+    
+    public Suspects getSuspect()
+    {
+        return Suspects;
+    }
+    
     // DO NOT DELETE, NEED BLANK CONSTRUCTOR TO READ FROM FILE
     Clue(){}
 
