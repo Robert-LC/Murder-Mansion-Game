34d1b9172b1904448d526dde9e697bdc8f41308c
meganikki2001 <82064919+meganikki2001@users.noreply.github.com>
2021-04-08 14:32:14 -0400

    Update Consumable.java

--- src/Consumable.java
+++ src/Consumable.java
@@ -1,5 +1,25 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: Consumable.java
+ * Project name: Murder Mansion Game
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Megan Toth, tothmn@etsu.edu
+ * Course:  CSCI 1260
+ * Creation Date: April 8, 2021
+ * ---------------------------------------------------------------------------
+*/
+
+/**
+ * Implements Consumable subclass of Item
+ *
+ * Date created: April 8, 2021
+ * 
+ * @author Megan Toth
+ */
 public class Consumable extends Item
 {
+    private String type;
+    
     // Constructor for consumable calls the Item Constructor.
     Consumable(String name, String description, String roomLocation)
     {
