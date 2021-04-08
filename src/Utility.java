5b383f4e89fcb4668e2557b39a659341a759d905
meganikki2001 <82064919+meganikki2001@users.noreply.github.com>
2021-04-08 14:34:39 -0400

    Update Utility.java

--- src/Utility.java
+++ src/Utility.java
@@ -1,12 +1,36 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: Utility.java
+ * Project name: Murder Mansion Game
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Megan Toth, tothmn@etsu.edu
+ * Course:  CSCI 1260
+ * Creation Date: April 8, 2021
+ * ---------------------------------------------------------------------------
+*/
 
+/**
+ * Implements Utility subclass of Item
+ *
+ * Date created: April 8, 2021
+ * 
+ * @author Megan Toth
+ */
 public class Utility extends Item
 {
+    private Room usableIn;
+    
     // Constructor for Utility calls the Item Constructor.
     Utility(String name, String description, String roomLocation)
     {
         super(name, description, roomLocation);
     }
-
+    
+    public Room getRoom()
+    {
+        return Room;
+    }
+    
     // DO NOT DELETE, NEED BLANK CONSTRUCTOR TO READ FROM FILE
     Utility(){}
 
