b514f2f33fb23e33fcba1fbc9d0e567400404335
chrisseals98 <chrisseals9893@gmail.com>
2021-04-09 15:39:56 -0400

    Update randomizeItems.java
    Forgot that we will be reading rooms from a txt file, so I changed  it so it can select rooms from an arraylist.

--- src/randomizeItems.java
+++ src/randomizeItems.java
@@ -4,33 +4,42 @@
  * Project Name: Final Project
  * ==========================================================================================
  * Creator's Name and Email: Chris Seals, sealscm@etsu.edu
- * Date Created: 04/08/2021
+ * Date Created: 04/09/2021
  * Course: CSCI-1260-942
+ * ==========================================================================================
  */
 
 import java.util.Random; //used to generate a random number to assign an item to a room
+import java.util.ArrayList; //used to get roomnames from a collection of rooms
 import java.util.HashMap; //used to access HashMap methods
 import java.util.Map; //used to iterate through hashmap
 
 /**
- * Assigns a hashmap of Item objects their room location by randomly selecting a room.
+ * Assigns a HashMap of Item objects their room location by randomly selecting
+ * a Room from an ArrayList.
  * 
  * @author Chris Seals
- * @since 04/08/2021
+ * @since 04/09/2021
  */
 public class randomizeItems
 {  
-    //Used to select a room
-    private static final String[] ROOMNAMES = {"Bedroom", "Library", "Garden", "Closet", "Main Hall", "Kitchen", "Bathroom", "Dining Hall", "Basement"};
-
-    public static void randomize(HashMap<String, Item> items)
+    /**
+     * Randomizes a HashMap of Item objects by assigning their room fields a random room name from the rooms arraylist.
+     * 
+     * @param items - HashMap of Items to be randomized
+     * @param rooms - Rooms to be randomly selected
+     */
+    public static void randomize(HashMap<String, Item> items, ArrayList<Room> rooms)
     {
         Random rand = new Random(); //used to generate a random int
 
         for(Map.Entry<String, Item> set:items.entrySet()) //iterates through the HashMap
         {
-            //sets the roomLocation field by a method call and passes it a string randomly selected from the list of names available
-            set.getValue().setRoomLocation(ROOMNAMES[rand.nextInt(9)]); 
+            /*
+            sets the roomLocation field by a getting the value of a set in the HashMap 
+            and passes it a string randomly selected from the list of names available
+            */
+            set.getValue().setRoomLocation(rooms.get(rand.nextInt(rooms.size())).getName()); 
         }
     }
 }//end randomizeItems
