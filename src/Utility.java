6ec4cbb858bb8aabbede806672cb20e4b61d6928
meganikki2001 <82064919+meganikki2001@users.noreply.github.com>
2021-04-22 20:25:45 -0400

    Update Utility.java

--- src/Utility.java
+++ src/Utility.java
@@ -28,7 +28,7 @@ public class Utility extends Item implements PointValue
     
     public int calcPoints()
     {
-        
+        return points;
     }
     
     // DO NOT DELETE, NEED BLANK CONSTRUCTOR TO READ FROM FILE
