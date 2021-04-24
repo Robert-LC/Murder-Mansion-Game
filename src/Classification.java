314bfb51d7ac9ec36feb5a41be9f06705a8e7edf
meganikki2001 <82064919+meganikki2001@users.noreply.github.com>
2021-04-23 23:00:14 -0400

    Update Classification.java

--- src/Classification.java
+++ src/Classification.java
@@ -1,3 +1,13 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: Classification.java
+ * Project name: Murder Mansion Game
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Megan Toth, tothmn@etsu.edu
+ * Course:  CSCI 1260
+ * Creation Date: April 23, 2021
+ * ---------------------------------------------------------------------------
+*/
 enum Classification
 {
     BEGINNER,
