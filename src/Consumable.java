33e8f679b0aa48869f45213075a64c43055c5614
meganikki2001 <82064919+meganikki2001@users.noreply.github.com>
2021-04-25 19:17:54 -0400

    Update Consumable.java

--- src/Consumable.java
+++ src/Consumable.java
@@ -32,7 +32,7 @@ public class Consumable extends Item implements PointValue
     }
 
     // DO NOT DELETE, NEED BLANK CONSTRUCTOR TO READ FROM FILE
-    Consumable(){}
+    Consumable(points){this.points = 10;}
 
 
     
