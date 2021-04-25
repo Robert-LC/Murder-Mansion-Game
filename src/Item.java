eb339f579acd57dafd69e31a7ff5a2400dae89cb
meganikki2001 <82064919+meganikki2001@users.noreply.github.com>
2021-04-25 19:57:19 -0400

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -2,7 +2,8 @@ public abstract class Item implements PointValue
 {
     private String name, description, roomLocation;
     int points;
-
+    
+    //main constructor that subclasses will inherit
     public Item(String name, String description, String roomLocation, int points)
     {
         this.name = name;
@@ -14,41 +15,49 @@ public abstract class Item implements PointValue
     // DO NOT DELETE, NEED BLANK CONSTRUCTOR TO READ FROM FILE
     public Item(){}
 
+    //getter for name
     public String getName()
     {
         return this.name;
     }
-
+    
+    //setter for name
     public void setName(String name)
     {
         this.name = name;
     }
-
+    
+    //getter for description
     public String getDescription()
     {
         return this.description;
     }
-
+    
+    //setter for description
     public void setDescription(String description)
     {
         this.description = description;
     }
-
+    
+    //getter for room location
     public String getRoomLocation()
     {
         return roomLocation;
     }
-
+    
+    //setter for room location
     public void setRoomLocation(String roomLocation)
     {
         this.roomLocation = roomLocation;
     }
     
+    //setter for points
     public void setPoints(int points)
     {
         this.points = 10;
     }
     
+    //getter for points
     public int getPoints()
     {
         return points;
