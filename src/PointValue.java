f0ea4458725b5d9ba42dfdfafe0ffe0ead6f9f5a
meganikki2001 <82064919+meganikki2001@users.noreply.github.com>
2021-04-26 19:16:52 -0400

    Update PointValue.java

--- src/PointValue.java
+++ src/PointValue.java
@@ -1,3 +1,15 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: Classification.java
+ * Project name: Murder Mansion Game
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Chris Seals, Megan Toth
+ * Course:  CSCI 1260
+ * Creation Date: April 23, 2021
+ * ---------------------------------------------------------------------------
+ */
+
+//interface for points
 public interface PointValue
 {
     public int calcPoints();
