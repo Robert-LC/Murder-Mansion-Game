29405a5bacc9532698975a197debbd1dad4424ae
sgc6512 <81432815+sgc6512@users.noreply.github.com>
2021-04-26 19:26:14 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //Load fxml file
         FXMLLoader loader = new FXMLLoader(getClass().getResource("TitleScreen.fxml"));
         Parent root = loader.load();
+        //Set title
         primaryStage.setTitle("Murder Mansion");
+        //Show the GUI
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
